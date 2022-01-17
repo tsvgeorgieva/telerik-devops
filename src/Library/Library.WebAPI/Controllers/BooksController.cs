6cc614304b4e20d798aa2511f9859f6d7226fe27
Tsvetina Georgieva <tsvetina.georgieva@bedegaming.com>
2022-01-17 22:12:05 +0200

    add simple project

---
+++ src/Library/Library.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,44 @@
+﻿using Library.WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Library.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return new Book[] { new Book { Author = "pesho", Name = "my book" } };
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            return new Book { Author = "pesho", Name = "my book" };
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Library/Library.WebAPI/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Library.WebAPI.Models
+{
+    public class Book
+    {
+        public string Author { get; set; }
+
+        public string Name { get; set; }
+    }
+}
